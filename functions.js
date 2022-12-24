b8cf2dceec88bbb9f5d6d365823306e9d2b5de7c
Krutik Rambhia <krutik.r@somaiya.edu>
2022-12-24 11:35:01 +0530

    drag trial

--- functions.js
+++ functions.js
@@ -1,6 +1,52 @@
-$(document).on('mousemove', function (e) {
-    basket.css('left', e.pageX);
+// $(basket).on('touchmove', function (e) {
+//     basket.css('left', e.pageX);
+//     // Object.keys(e).forEach((prop)=> console.log(prop + ' -> ' + e[prop]));
+// });
+
+const draggableElement = document.getElementById('basket');
+
+let isDragging = false;
+let currentX;
+let currentY;
+let initialX;
+let initialY;
+let xOffset = 0;
+let yOffset = 0;
+
+draggableElement.addEventListener('dragstart', function(event) {
+  initialX = event.clientX - xOffset;
+  initialY = event.clientY - yOffset;
+
+  if (event.target === draggableElement) {
+    isDragging = true;
+  }
 });
+
+draggableElement.addEventListener('drag', function(event) {
+  if (isDragging) {
+    event.preventDefault();
+    currentX = event.clientX + initialX;
+    currentY = initialY;
+
+    xOffset = currentX;
+    yOffset = currentY;
+
+    setTranslate(currentX, draggableElement);
+  }
+});
+
+draggableElement.addEventListener('dragend', function(event) {
+  initialX = currentX;
+  initialY = currentY;
+
+  isDragging = false;
+});
+
+function setTranslate(xPos, el) {
+  el.style.left = xPos + 'px';
+//   el.style.top = yPos + 'px';
+}
+
 
 function egg_down(egg) {
     egg_current_position = parseInt(egg.css('top'));
@@ -18,6 +64,14 @@ function check_egg_hits_floor(egg) {
 
 function set_egg_to_initial_position(egg) {
     egg.css('top', egg_initial_position);
+    if(egg.selector == '#egg1'){
+        egg.css('left',(Math.random()*20)+'%')    
+    }else if(egg.selector == '#egg2'){
+        egg.css('left',((Math.random()*30)+30)+'%')
+    }else{
+        egg.css('left',((Math.random()*30)+60)+'%')
+    }
+    
 }
 
 function show_bulls_eye(egg) {
@@ -38,7 +92,7 @@ function decrement_life() {
 }
 
 function check_egg_hits_basket(egg) {
-    if (collision(egg, dish)) {
+    if (collision(egg, basket)) {
         egg_top = parseInt(egg.css('top'));
         if (egg_top < basket_top) {
             update_score();
